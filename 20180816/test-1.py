bdc7434ba2bf5367119d0e3b97adb09b074e0f9d
znn1414 <znn1414@sina.com>
2018-08-16 22:47:36 +0800

    更新

--- 20180816/test-1.py
+++ 20180816/test-1.py
@@ -2,3 +2,17 @@
 
 spam = 40
 print(spam)
+
+
+print('hello world!')
+print('what is your name?')
+
+myName = input()
+print('It is good to meet you, ' + myName)
+print('The length of your name is:')
+print(len(myName))
+
+print('What is your age?')
+myAge = input()
+
+print('You will be ' + str(int(myAge) + 1 ) + ' in a year.')
